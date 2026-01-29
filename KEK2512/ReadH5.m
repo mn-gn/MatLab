35518f872a8e869d1359529cc285f0cba6b077d4
mngn <mngn8526@gmail.com>
2026-01-29 10:08:10 +0900

    <add> palxfel2409 folder / AI_distribution.m

--- KEK2512/ReadH5.m
+++ KEK2512/ReadH5.m
@@ -1,58 +1,77 @@
 clc; clear; close all;
 
-% A = zeros(13,2048,2048);
-
-filename = "Z:\202512KEK_h5\run4.h5";
-data_path_neg = "/delay/delay_p1.50e-10/neg";
-data_path_on = "/delay/delay_p1.50e-10/neg";
-
-tic
-T_neg = h5read(filename, data_path_neg);
-T_on = h5read(filename,data_path_on);
-toc
-
-T_neg = double(T_neg);
-T_on = double(T_on);
-
-reference = squeeze(mean(T_neg,1));
-
-%% MEAN PLOT
-close all;
-figure;
-
-imagesc(squeeze(T_neg(100,:,:)));
-colormap('gray');
-axis image
-
-for i = 1024 : 1024
-    for j = 1 : 50
-        figure;     
-        A = reference(i,j*20);
-        B = mean(T_on(:,i-2:i+2,j*20-2:j*20+2),[2,3]);
-        C = B-A;
-
-        histogram(B,100);
-        hold on;
-        xline(mean(B),'--r','LineWidth',2);
-        hold off;
-    end
+%% PYTHON LIBRARY LOAD
+try
+    fabio = py.importlib.import_module('fabio');
+    detectors = py.importlib.import_module('pyFAI.detectors');
+    azimuthal = py.importlib.import_module('pyFAI.azimuthalIntegrator');
+    disp('Python libraries loaded.');
+catch
+    error('파이썬 라이브러리 로드 실패. pyFAI/fabio 설치 확인 필요.');
 end
 
-%% PIXEL PLOT
+%% DATA LOAD
+filename = "Z:\202512KEK_MeOH_h5\run4.h5";
+data_path_p150p_ref = "/delay/delay_p1.50e-10/neg";
+data_path_p150p     = "/delay/delay_p1.50e-10/on";
+
+D_p150p_ref = h5read(filename, data_path_p150p_ref);
+D_p150p     = h5read(filename,data_path_p150p);
+
+D_p150p_ref = double(permute(D_p150p_ref,[3 2 1]));
+D_p150p     = double(permute(D_p150p, [3 2 1]));
+
+poni = read_poni("Detector_poni.poni");
+mask = read_mask("Mask_run4.edf");
+
+%% GEOMETRY
+
+pixel_size1 = poni.PixelSize1;
+pixel_size2 = poni.PixelSize2;
+dist = poni.Distance;
+poni1 = poni.Poni1;
+poni2 = poni.Poni2;
+rot1 = poni.Rot1;
+rot2 = poni.Rot2;
+rot3 = poni.Rot3;
+wavelength = poni.Wavelength;
+
+%% AI ENGINE START
+det_shape = py.tuple({int32(2048), int32(2048)});
+detector = detectors.Detector(pixel_size1, pixel_size2, pyargs('max_shape', det_shape));
+
+ai_params = pyargs(...
+    'dist', dist, 'poni1', poni1, 'poni2', poni2, ...
+    'rot1', rot1, 'rot2', rot2, 'rot3', rot3, ...
+    'detector', detector, 'wavelength', wavelength);
+
+ai = azimuthal.AzimuthalIntegrator(ai_params);
+
+%% INTEGRATE
+I_p150p_ref = zeros(1024,373);
+I_p150p     = zeros(1024, 373);
+
+for i = 1 : size(I_p150p_ref,2)
+    res1 = ai.integrate1d(D_p150p_ref(:,:,i), int32(1024), ...
+    pyargs('unit', 'q_A^-1', 'polarization_factor', 0.99, 'mask', mask, 'method', 'median'));
+    I_p150p_ref(:,i) = double(res1{2}.tolist())';
+
+    res2 = ai.integrate1d(D_p150p(:,:,i), int32(1024), ...
+    pyargs('unit', 'q_A^-1', 'polarization_factor', 0.99, 'mask', mask, 'method', 'median'));
+    I_p150p(:,i) = double(res2{2}.tolist())';
+end
+
+DS_p150p = I_p150p - I_p150p_ref;
+
+q = double(res1{1}.tolist())';
+
+M = mean(DS_p150p,2);
+
+%% DISTRIBUTION
 close all;
-figure;
-
-imagesc(squeeze(T_neg(100,:,:)));
-colormap('gray');
-axis image
-
-for i = 1024 : 1024
-    for j = 1 : 50
-        figure;     
-        B = T_on(:,i,j*20);
-        histogram(B,100);
-        hold on;
-        xline(mean(B),'--r','LineWidth',2);
-        hold off;
-    end
+for i = 4 : 47
+    figure; hold on;
+    histogram(DS_p150p(20*i,:),373);
+    xline(M(20*i),'r:','LineWidth',2);
+    hold off
 end
---
+++ PALXFEL2409_I3_MeOH/AI_Distribution.m
@@ -0,0 +1,62 @@
+clc; clear; close all;
+
+tth = h5read("Z:\PAL_XFEL_2409\rawData\I3_MeOH_00002_DIR\eh1rayMXAI_tth\001_001_001.h5","/1725167840.1521745_5784");
+
+
+folder = "Z:\PAL_XFEL_2409\rawData\I3_MeOH_00002_DIR\eh1rayMXAI_int";
+h5dir = dir(sprintf("%s/*.h5",folder));
+
+% for 1~size
+
+h5name = h5dir(1).name;
+filename = sprintf("%s/%s",folder,h5name);
+h5 = h5info(filename);
+
+I = zeros(size(tth,1),size(h5.Datasets,1));
+for i = 1 : size(h5.Datasets,1)
+    shot_name = h5.Datasets(i).Name;
+
+    I(:,i) = h5read(filename,sprintf("/%s",shot_name));
+end
+
+%% PLOT I (BEFORE PROCESSING)
+I_mean = mean(I,2);
+I_std = std(I,0,2);
+close all 
+for i = 1 : 20
+    figure; hold on;
+    title(sprintf("2\\theta=%f, \\sigma=%f",tth(20*i),I_std(20*i)));
+    histogram(I(20*i,:),100);
+    xline(I_mean(20*i),'r:','LineWidth',3);
+    hold off;
+end
+figure;
+surf(tth,1:1:500,I');
+shading flat
+xlabel("2\theta"); ylabel("index"); zlabel("intensity");
+colormap turbo
+
+%% AUC NORMAILZATION
+
+norm_range = [18.9; 37.55];
+norm_idx = find(tth>=norm_range(1)&tth<=norm_range(2));
+
+norm_factors = sum(I(norm_idx,:),1);
+I_norm = I ./ norm_factors;
+
+%% PLOT I (AUC NORMALIZED)
+close all
+I_norm_mean = mean(I_norm,2);
+I_norm_std = std(I_norm,0,2);
+for i = 1 : 20
+    figure; hold on;
+    title(sprintf("2\\theta=%f, \\sigma=%f",tth(20*i),I_norm_std(20*i)));
+    histogram(I_norm(20*i,:),100);
+    xline(I_norm_mean(20*i),'r:','LineWidth',3);
+    hold off;
+end
+figure;
+surf(tth,1:1:500,I_norm');
+shading flat
+xlabel("2\theta"); ylabel("index"); zlabel("intensity");
+colormap turbo
